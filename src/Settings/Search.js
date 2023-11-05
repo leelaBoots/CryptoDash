2c87f04bfaf3cb46222880e395f580f1f5a63734
movecotton <31789819+leelaBoots@users.noreply.github.com>
2023-11-05 10:54:10 -0800

    Fixed bug with fuzzy search

--- src/Settings/Search.js
+++ src/Settings/Search.js
@@ -22,17 +22,17 @@ const handleFilter = _.debounce((inputValue, coinList, setFilteredCoins) => { //
   // Get all the coin symbols
   let coinSymbols = Object.keys(coinList);
   // Get all the coin names, map symbol to names
-  let coinNames = coinSymbols.map(sym => coinList[sym].CoinName)
+  let coinNames = coinSymbols.map(sym => coinList[sym].CoinName);
   // combine list of symbols and names together to search on
   let allStringsToSearch = coinSymbols.concat(coinNames);
   // use fuzzy library to search, take the data structure returned from fuzzy and mao to the stringa we want (name and/or symbol)
   let fuzzyResults = fuzzy
     .filter(inputValue, allStringsToSearch, {})
-    .map(result => result.string)
+    .map(result => result.string);
 
   // use the results to iterate over our original list of coins to build a new list of coins that match name or symbol
   let filteredCoins = _.pickBy(coinList, (result, symKey) => {
-    let coinName = result.coinName;
+    let coinName = result.CoinName;
     // do our fuzzy results return the symbol, or do they contain the name.
     return (_.includes(fuzzyResults, symKey) || _.includes(fuzzyResults, coinName));
   })  
@@ -41,8 +41,8 @@ const handleFilter = _.debounce((inputValue, coinList, setFilteredCoins) => { //
 }, 500);
 
 
-function filterCoins(event, setFilteredCoins, coinList) {
-  let inputValue = event.target.value;
+function filterCoins(e, setFilteredCoins, coinList) {
+  let inputValue = e.target.value;
   // if the search input is cleared out, set filter to null, so that all the coins will show up again
   if (!inputValue) {
     setFilteredCoins(null);
@@ -52,12 +52,13 @@ function filterCoins(event, setFilteredCoins, coinList) {
 }
 
 export default function _Search() {
+  // e is an event that gets fired offon keyup
   return (
     <AppContext.Consumer>
       {({setFilteredCoins, coinList}) =>
         <SearchGrid>
           <h2>Search all coins</h2>
-          <SearchInput onKeyUp={(event) => filterCoins(event, setFilteredCoins, coinList)} />
+          <SearchInput onKeyUp={(e) => filterCoins(e, setFilteredCoins, coinList)} />
         </SearchGrid>
       }
     </AppContext.Consumer>
